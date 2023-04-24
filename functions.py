05fe37facc89d72f08ef09598e0a15eea46f7a22
nabielad <117137444+nabielad@users.noreply.github.com>
2023-04-24 18:09:17 +0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,3 +1,4 @@
+# FUNGSI PENGGANTI LEN
 def length(list):
     count = 0
     i = 0
@@ -10,6 +11,7 @@ def length(list):
 
     return count
 
+# FUNGSI PENGGANTI APPEND
 def newappend(list, item):
     i = 0
     while True:
@@ -20,3 +22,15 @@ def newappend(list, item):
             break
 
     return list
+
+# FUNGSI PENGGANTI POP
+def newpop(list, item):
+    newlist = ["" for i in range(length(list)-1)]
+    index = 0   # inisialisasi increment untuk newlist
+    for i in range(length(list)):
+        if list[i] != item:
+            newlist[index] = list[i]
+            index += 1
+        # else (list[i] == item) maka tidak akan masuk ke dalam list baru
+
+    return newlist
